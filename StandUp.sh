fe4417d7dbcea4bc498bb460ef6479edcadd2dc3
marfoldi <marfoldi_@hotmail.com>
2015-11-10 02:13:58 +0100

    Added a zenity notification

--- StandUp.sh
+++ StandUp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 while :
 do
-  echo 'WAKE UP, NEO...'
-  mplayer bells.mp3 > /dev/null 2>&1
+  mplayer bells.mp3 > /dev/null 2>&1 | echo 'message:WAKE UP, NEO...' | zenity --notification --listen
   sleep 20m
 done
