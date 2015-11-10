ee372f5d33cb8216cb074fca4c748772180e027c
marfoldi <marfoldi_@hotmail.com>
2015-11-10 02:30:22 +0100

    Added a timeout for the zenity notification

--- StandUp.sh
+++ StandUp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 while :
 do
-  mplayer bells.mp3 > /dev/null 2>&1 | echo 'message:WAKE UP, NEO...' | zenity --notification --listen
-  sleep 20m
+  mplayer bells.mp3 > /dev/null 2>&1 | echo 'message:WAKE UP, NEO...' | zenity --notification --listen --timeout=5 | sleep 20m
 done
